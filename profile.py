480d817cc1b19c7648d74de675bdbe62f879e116
May Hoang <mayzhoang@gmail.com>
2026-06-17 10:41:59 -0700

    Add Python profile variables lab

---
+++ profile.py
@@ -0,0 +1,19 @@
+full_name = "May Z Hoang"
+
+nickname = "Mai"
+
+age = 19
+
+used_python_before = False 
+
+hobbies = ["gaming", "hanging out with friends", "watching movies"]
+
+favories = { 
+"movie":"Obsession",
+"food": "Zha Jiang Mian",
+"hobby": "gaming"
+}
+
+print (full_name, age, hobbies)
+
+languages_spoken = {"English", "Cantonese"}
